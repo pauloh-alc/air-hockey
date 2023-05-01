deeb4a376c44202bbfcc627d04d57d01ee2c3fc2
Paulo Alencar <pauloalencar.aluno@gmail.com>
2023-05-01 16:52:48 -0300

    Fix no arquivo colors
    O motivo foi que as diretivas de processamento estava ainda em
    português.

--- air-hockey/include/colors.h
+++ air-hockey/include/colors.h
@@ -1,5 +1,5 @@
-#ifndef CORES_H
-#define CORES_H
+#ifndef COLORS_H
+#define COLORS_H
 
 #include <GL/glut.h>
 
@@ -7,7 +7,8 @@ const GLfloat RED[] = { 1.0f, 0.0f, 0.0f };
 const GLfloat GREEN[] = { 0.0f, 1.0f, 0.0f };
 const GLfloat BLUE[] = { 0.0f, 0.0f, 1.0f };
 const GLfloat WHITE[] = { 1.0f, 1.0f, 1.0f };
+const GLfloat BLACK[] = { 0.0f, 0.0f, 0.0f };
 
 const GLclampf BACKGROUND_COLOR_WINDOW[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 
-#endif // CORES_H
+#endif // COLORS_H
