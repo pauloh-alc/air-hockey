ee07d4b11e656fc3a14f1256a49e72c8f3ae9b7d
Paulo Alencar <pauloalencar.aluno@gmail.com>
2023-05-13 11:33:28 -0300

    Cria idea inicial para classe AI
    A mudança foi necessária para criar a estrutura inicial
    do jogador 2.

--- air-hockey/include/ai.h
+++ air-hockey/include/ai.h
@@ -1,9 +1,18 @@
 #ifndef AI_H
 #define AI_H
+#include "glm/glm.hpp"
 
 class AI {
-public:
-  AI();
+    private:
+        glm::vec2 position;
+        float radius;
+        glm::vec3 color;
+        float size;
+        float speed;
+    public:
+        AI();
+        void draw();
+
 };
 
 #endif // AI_H
--- air-hockey/src/game.cpp
+++ air-hockey/src/game.cpp
@@ -1,3 +1,5 @@
 #include "game.h"
 
-Game::Game() {}
+Game::Game() {
+
+}
