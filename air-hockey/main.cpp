ebdf5fd8e89373af077f4ff87f60724675f6ac20
Paulo Alencar <pauloalencar.aluno@gmail.com>
2023-05-21 16:43:22 -0300

    Melhorando a colisão entre Player1 e puck.

--- air-hockey/main.cpp
+++ air-hockey/main.cpp
@@ -48,6 +48,31 @@ void set_projection() {
     glLoadIdentity();
 }
 
+void release_key(int key, int x, int y) {
+    keys.erase(key);
+}
+
+void timer(int v){
+    glutTimerFunc(1000.0/FPS, timer, 0);
+    puck.move_puck(0.7);
+    glutPostRedisplay();
+}
+
+void verify_collision() {
+    if (player1.check_collision(puck)) {
+        float player1_pos_x = player1.get_position_x();
+        float player1_pos_y = player1.get_position_y();
+
+        float puck_pos_x = puck.get_position_x();
+        float puck_pos_y = puck.get_position_y();
+
+        float direction_x = puck_pos_x - player1_pos_x;
+        float direction_y = puck_pos_y - player1_pos_y;
+
+        puck.set_direction(direction_x, direction_y);
+    }
+}
+
 void move_player1(int key, int x, int y) {
     keys.insert(key);
 
@@ -72,32 +97,11 @@ void move_player1(int key, int x, int y) {
     player1.set_position_x(player1_x);
     player1.set_position_y(player1_y);
 
-    glutPostRedisplay();
-}
-
-void release_key(int key, int x, int y) {
-    keys.erase(key);
-}
-
-//Função que será executada periodicamente (serve para realizar as modificações entre um frame e o próximo da animação)
-void timer(int v){
-    //A função glutTimerFunc define que a função 'timer' irá executar novamente daqui a uma certa quantidade de tempo em milissegundos.
-    //Se deseja executar 60 frames por segundo, o tempo entre um frame e o próximo é 1000/60 milissegundos
-    //Atenção! Não chame timer dentro de timer. Isso criará uma recursão infinita.
-    glutTimerFunc(1000.0/FPS, timer, 0);
-
-    //Realize as modificações entre um frame e o próximo aqui
-    //Como o objeto B move-se uma distância maior do que A entre cada frame, ele irá parecer andar mais rápido na tela
-    puck.move_puck(0.3);
-
-    //Após as modificações, a tela deve ser atualizada
-    glutPostRedisplay();
-}
-
-void verify_collision() {
     if (player1.check_collision(puck)) {
-        glutTimerFunc(1000.0/FPS, timer, 0);
+        verify_collision();
     }
+
+    glutPostRedisplay();
 }
 
 void draw() {
@@ -107,7 +111,6 @@ void draw() {
     puck.draw();
     player1.draw();
     player2.draw();
-    verify_collision();
     goal1.set_position_y(14.0f);
     goal1.draw();
     goal2.set_position_y(HEIGHT_AREA - 14.0f);
@@ -122,6 +125,7 @@ int main(int argc, char** argv) {
     glutDisplayFunc(draw);
     glutSpecialFunc(move_player1);
     glutSpecialUpFunc(release_key);
+    glutTimerFunc(1000.0/FPS, timer, 0);
     glutMainLoop();
 
     return 0;
