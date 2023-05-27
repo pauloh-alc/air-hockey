f4df5f30e3c6f69a3498af318df9d19d6af64c1a
Yan Rodrigues <ccyan05@alu.ufc.br>
2023-05-27 16:21:23 -0300

    Refactor:
    Formatando o código no estilo definido.

--- air-hockey/include/player.h
+++ air-hockey/include/player.h
@@ -4,43 +4,31 @@
 #include "puck.h"
 
 class Player {
-    private:
-        glm::vec2 position;
-        float radius;
-        glm::vec3 color;
-        float size;
-        float speed;
+private:
+  glm::vec2 position;
+  float radius;
+  glm::vec3 color;
+  float size;
+  float speed;
 
-    public:
-        Player();
+public:
+  Player();
 
-        float get_position_x() {
-            return position.x;
-        }
+  float get_position_x() { return position.x; }
 
-        float get_position_y() {
-            return position.y;
-        }
+  float get_position_y() { return position.y; }
 
-        float get_radius() {
-            return radius;
-        }
+  float get_radius() { return radius; }
 
-        float get_size() {
-            return size;
-        }
+  float get_size() { return size; }
 
-        void set_position_x(float x) {
-            position.x = x;
-        }
+  void set_position_x(float x) { position.x = x; }
 
-        void set_position_y(float y) {
-            position.y = y;
-        }
+  void set_position_y(float y) { position.y = y; }
 
-        void set_position_x();
-        void draw();
-        bool check_collision(Puck& puck);
+  void set_position_x();
+  void draw();
+  bool check_collision(Puck &puck);
 };
 
 #endif // PLAYER_H
