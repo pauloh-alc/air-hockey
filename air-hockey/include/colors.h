e00ee5e76f3f181911b533d172a0b18b2af5e45f
Yan Rodrigues <ccyan05@alu.ufc.br>
2023-05-27 16:18:56 -0300

    Refactor:
    Criando uma nova cor verde para o campo.

--- air-hockey/include/colors.h
+++ air-hockey/include/colors.h
@@ -9,6 +9,7 @@ const GLfloat BLUE[] = {0.0f, 0.0f, 1.0f};
 const GLfloat WHITE[] = {1.0f, 1.0f, 1.0f};
 const GLfloat BLACK[] = {0.0f, 0.0f, 0.0f};
 const GLfloat YELLOW[] = {1.0f, 1.0f, 0.0f};
+const GLfloat GREEN_FIELD[] = {10.0f / 255, 127.0f / 255, 12.0f / 255};
 
 const GLclampf BACKGROUND_COLOR_WINDOW[] = {1.0f, 1.0f, 1.0f, 1.0f};
 
