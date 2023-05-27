3ee2a8196bbd0f2685d5d099fe9418d1847a28ea
Yan Rodrigues <ccyan05@alu.ufc.br>
2023-05-27 16:25:54 -0300

    Refactor:
    Formatando o arquivo Goal para o estilo definido.

--- air-hockey/src/goal.cpp
+++ air-hockey/src/goal.cpp
@@ -4,17 +4,17 @@
 #include "sizes.h"
 
 Goal::Goal() {
-    position = glm::vec2(WIDTH_AREA / 2.0f, 14.0f);
-    width = 100;
-    lenght = 10;
-    background_color = glm::vec3(YELLOW[0], YELLOW[1], YELLOW[2]);
+  position = glm::vec2(WIDTH_AREA / 2.0f, 14.0f);
+  width = 100;
+  lenght = 10;
+  background_color = glm::vec3(YELLOW[0], YELLOW[1], YELLOW[2]);
 }
 
 void Goal::draw() {
-    glPushMatrix();
-        glTranslatef(position.x, position.y, 0);
-        glScalef(width, lenght, 1);
-        glColor3f(background_color.r, background_color.g, background_color.b);
-        Shapes::square();
-    glPopMatrix();
+  glPushMatrix();
+  glTranslatef(position.x, position.y, 0);
+  glScalef(width, lenght, 1);
+  glColor3f(background_color.r, background_color.g, background_color.b);
+  Shapes::square();
+  glPopMatrix();
 }
