b8cbb396bc9ce13f88d5b4ace9a3c5fd8f68f717
Paulo Alencar <pauloalencar.aluno@gmail.com>
2023-05-28 04:46:38 -0300

    style, corrigindo tabulacao de alguns file
    Por o trabalho ser em equipe alguns acabam seguinte padroes
    de tabulacoes diferentes. Decidimos adotar padrão de tabulacao
    com 2 espacos

--- air-hockey/include/ai.h
+++ air-hockey/include/ai.h
@@ -4,29 +4,29 @@
 #include "puck.h"
 
 enum{
-    LEFT = 1,
-    RIGHT
+  LEFT = 1,
+  RIGHT
 };
 
 class AI {
-    private:
-        glm::vec2 position;
-        int direction;
-        float radius;
-        glm::vec3 color;
-        float size;
-        float speed;
+  private:
+    glm::vec2 position;
+    int direction;
+    float radius;
+    glm::vec3 color;
+    float size;
+    float speed;
 
-    public:
-        AI();
-        void draw();
-        void move(float distantion);
+  public:
+    AI();
+    void draw();
+    void move(float distantion);
 
-        float get_position_x() { return position.x; }
+    float get_position_x() { return position.x; }
 
-        float get_position_y() { return position.y; }
+    float get_position_y() { return position.y; }
 
-        bool check_collision(Puck &puck);
+    bool check_collision(Puck &puck);
 };
 
 #endif // AI_H
